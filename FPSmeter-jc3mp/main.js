5acd5c6cec8ddc880e383bcec32a8ef2b35c96ec
Alexander Birkner <alex.birkner@gmail.com>
2016-12-30 13:13:09 +0100

    Return a command id to retrieve the correct response to a command

--- FPSmeter-jc3mp/main.js
+++ FPSmeter-jc3mp/main.js
@@ -21,15 +21,15 @@ ws.createServer(function (conn) {
 
         if (json.command == 'login') {
             if (conn.auth) {
-                conn.sendText(JSON.stringify({success: true, command: json.command, message: 'You are already logged in.'}));
+                conn.sendText(JSON.stringify({success: true, id: json.id, command: json.command, message: 'You are already logged in.'}));
             } else {
                 if (json.password === config.password) {
                     console.log('[FPSmeter] Login successful.');
-                    conn.sendText(JSON.stringify({success: true, command: json.command, message: 'Login was successful.'}));
+                    conn.sendText(JSON.stringify({success: true, id: json.id, command: json.command, message: 'Login was successful.'}));
                     conn.auth = true;
                 } else {
                     console.log('[FPSmeter] Login failed. Wrong password.');
-                    conn.sendText(JSON.stringify({success: false, command: json.command, message: 'Login failed. Wrong password.'}));
+                    conn.sendText(JSON.stringify({success: false, id: json.id, command: json.command, message: 'Login failed. Wrong password.'}));
                 }
             }
         } else {
@@ -40,14 +40,14 @@ ws.createServer(function (conn) {
                     let maxFPS = JSON.parse(jcmp.server.config).maxTickRate;
 
                     console.log("[FPSmeter] Serverinfo - currentFPS: " + currentFPS + " currentPlayers: " + currentPlayers + " maxFPS: " + maxFPS);
-                    conn.sendText(JSON.stringify({success: true, command: json.command, currentFPS: currentFPS, currentPlayers: currentPlayers, maxFPS: maxFPS}));
+                    conn.sendText(JSON.stringify({success: true, id: json.id, command: json.command, currentFPS: currentFPS, currentPlayers: currentPlayers, maxFPS: maxFPS}));
                 } else {
                     console.log('[FPSmeter] Unkwown command "' + json.command + '".');
-                    conn.sendText(JSON.stringify({success: false, command: json.command, message: 'Unkwown command "' + json.command + '".'}));
+                    conn.sendText(JSON.stringify({success: false, id: json.id, command: json.command, message: 'Unkwown command "' + json.command + '".'}));
                 }
             } else {
                 console.log('[FPSmeter] Please login first.');
-                conn.sendText(JSON.stringify({success: false, command: json.command, message: 'Please login first.'}));
+                conn.sendText(JSON.stringify({success: false, id: json.id, command: json.command, message: 'Please login first.'}));
             }
         }
     });
--- sample_server.rb
+++ sample_server.rb
@@ -1,15 +1,18 @@
 require 'websocket-client-simple'
 require 'json'
+require 'securerandom'
 
 ws = WebSocket::Client::Simple.connect 'ws://127.0.0.1:11100'
 ws.on :message do |msg|
   parsed = JSON.parse(msg.data)
   if parsed['success'] and parsed['command'] == 'serverinfo'
     puts "Current FPS: #{parsed['currentFPS']}, Current Players: #{parsed['currentPlayers']}, Max FPS: #{parsed['maxFPS']}"
+  else
+    puts parsed
   end
 end
 
 ws.on :open do
-  ws.send(JSON.dump({:command => 'login', :password => 'mySpecialPassword'}))
-  ws.send(JSON.dump({:command => 'serverinfo'}))
+  ws.send(JSON.dump({:command => 'login', :id => SecureRandom.hex, :password => 'mySpecialPassword'}))
+  ws.send(JSON.dump({:command => 'serverinfo', :id => SecureRandom.hex}))
 end
